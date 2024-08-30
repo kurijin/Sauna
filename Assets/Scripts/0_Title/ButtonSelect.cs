669a72baff0f9d23eea17f65c59cf73667568dd7
栗田 祥河 <ctwk0906@mail4.doshisha.ac.jp>
2024-08-30 23:06:26 +0900

    --タイトル画面の修正

--- Assets/Scripts/0_Title/ButtonSelect.cs
+++ Assets/Scripts/0_Title/ButtonSelect.cs
@@ -6,25 +6,47 @@ using UnityEngine;
 
 /// </<summary>
 /// タイトルシーンのボタン管理全部
-/// Canvasは合計4つ（タイトル、クレジット、モードセレクト、難易度選択)
+/// Canvasは合計5つ（タイトル、クレジット、モードセレクト、難易度選択,遊び方)
 /// </summary>
+/// /// 
 public class ButtonSelect : MonoBehaviour
 {
     [SerializeField] private GameObject Title;
     [SerializeField] private GameObject credit;
     [SerializeField] private GameObject modeSelect;    
     [SerializeField] private GameObject difficulySelect;
+    [SerializeField] private GameObject HowtoPlay;
 
     /// ボタンの音声
     [SerializeField] private SoundManager soundManager;
     [SerializeField] private AudioClip buttonSE;
 
-    public void OnClickStart()
+
+    //最初の画面のPlay
+    public void OnClickPlayButton()
     {
         soundManager.PlaySE(buttonSE);
         Title.SetActive(false);
         modeSelect.SetActive(true);
     }
+    //クレジットボタン
+    public void OnClickCreditUI()
+    {
+        soundManager.PlaySE(buttonSE);
+        Title.SetActive(false);
+        credit.SetActive(true);
+    }
+    //遊び方ボタン
+    public void OnClickHowtoPlay()
+    {
+        soundManager.PlaySE(buttonSE);
+        Title.SetActive(false);
+        HowtoPlay.SetActive(true);
+    }
+    
+
+    //モード選択
+    //チャレンジモードボタン
     public void OnClickChallenge()
     {
         soundManager.PlaySE(buttonSE);
@@ -32,19 +54,25 @@ public class ButtonSelect : MonoBehaviour
         difficulySelect.SetActive(true);
     }
 
+    //エンドレスボタン
     public void OnClickEndless()
     {
         soundManager.PlaySE(buttonSE);
         SceneChanger.Instance.ToMainScene();
     }
 
+    //クレジット、遊び方、モード選択共通のBackボタン
     public void OnClickBacktoTitle()
     {
         soundManager.PlaySE(buttonSE);
         Title.SetActive(true);
         modeSelect.SetActive(false);
+        credit.SetActive(false);
+        HowtoPlay.SetActive(false);
     }
 
+    //難易度選択
+    //モード選択に戻る
     public void OnClickBacktoMode()
     {
         soundManager.PlaySE(buttonSE);
@@ -52,20 +80,6 @@ public class ButtonSelect : MonoBehaviour
         difficulySelect.SetActive(false);
     }
 
-    public void OnClickCreditUI()
-    {
-        soundManager.PlaySE(buttonSE);
-        Title.SetActive(false);
-        credit.SetActive(true);
-    }
-
-    public void OnClickBackCreditUI()
-    {
-        soundManager.PlaySE(buttonSE);
-        Title.SetActive(true);
-        credit.SetActive(false);
-    }
-    
     /// <summary>
     /// 難易度選択後の処理
     /// </summary>
@@ -79,7 +93,7 @@ public class ButtonSelect : MonoBehaviour
         soundManager.PlaySE(buttonSE);
     }
 
-    public void OnClickDifficult()
+    public void OnClickHard()
     {
         soundManager.PlaySE(buttonSE);
     }
